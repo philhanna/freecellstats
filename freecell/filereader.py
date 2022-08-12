ca297a12952acec8fa70b70bff169aabbf981834
Phil Hanna <ph1204@gmail.com>
2022-08-12 17:17:48 -0400

    Split old mainline into individual classes

---
+++ freecell/filereader.py
@@ -0,0 +1,59 @@
+import os
+import sys
+
+from freecell.statistics import Statistics
+
+
+class FileReader:
+    '''
+    Reads raw data from the XML file that contains the history
+    '''
+
+    INPUT_FILE = os.path.expanduser('~/.gconf/apps/aisleriot/%gconf.xml')
+
+    @staticmethod
+    def getChildText(node):
+        '''
+        Returns the concatenated text of all child nodes
+        '''
+        sb = ''
+        for child in node.childNodes:
+            sb = sb + child.nodeValue
+        return sb
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        inputFile = FileReader.INPUT_FILE
+        if not os.path.exists(inputFile):
+            print('{0} input file does not exist'.format(inputFile))
+            sys.exit()
+
+        dom = xml.dom.minidom.parse(inputFile)
+        elemGconf = dom.documentElement
+        elemEntry = None
+        nodelist = elemGconf.getElementsByTagName('entry')
+        for node in nodelist:
+            if node.getAttribute('name') == 'statistics':
+                elemEntry = node
+        if not elemEntry:
+            print('No statistics found')
+            sys.exit()
+
+        #   Get the wins, total, best time, and worst time
+
+        nodes = elemEntry.getElementsByTagName('stringvalue')
+
+        wins    = int(FileReader.getChildText(nodes[1]))
+        total   = int(FileReader.getChildText(nodes[2]))
+        best    = int(FileReader.getChildText(nodes[3]))
+        worst   = int(FileReader.getChildText(nodes[4]))
+
+        #   Create the statistics object
+
+        self.stats = Statistics(wins, total, best, worst)
+
+    def __str__(self):
+        return str(self.stats)
+
