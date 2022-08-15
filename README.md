f186dfa5c4d1247bf9e0249c9e0a18a04595135a
Phil Hanna <ph1204@gmail.com>
2022-08-15 12:40:30 -0400

    Added setup.py
    Also updated README.md with repository location

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+Source: [https://github.com/philhanna/freecellstats](https://github.com/philhanna/freecellstats)
 # freecellstats
 Command line tool to display the statistics for Aisleriot Freecell
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='freecellstats',
+    version='1.0.0',
+    packages=['tests', 'tests.testdata', 'freecell'],
+    url='https://github.com/philhanna/freecellstats',
+    license='MIT',
+    author='saspeh',
+    author_email='ph1204@gmail.com',
+    description='Displays freecell statistics'
+)
