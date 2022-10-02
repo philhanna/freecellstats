f9fa2d1001a1c7ec5fa16bdef431f996b6ae2154
Phil Hanna <ph1204@gmail.com>
2022-10-01 22:19:34 -0400

    Bumped version number to 1.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='freecellstats',
-    version='1.2.0',
+    version='1.2.1',
     packages=['freecell'],
     url='https://github.com/philhanna/freecellstats',
     license='MIT',
