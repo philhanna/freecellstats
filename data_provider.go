9e2b264e9a2c98f913528883ca871310fd54d563
Phil Hanna <ph1204@gmail.com>
2023-02-05 15:43:59 -0500

    Use mock object to provide test data

--- data_provider.go
+++ data_provider.go
@@ -14,26 +14,38 @@ const (
 type DataProvider struct {
 }
 
-type ConfigStringGetter func() string
-
-func DefaultConfigGetter() string {
-	home, _ := os.UserHomeDir()
-	configFile := filepath.Join(home, CONFIG_FILE_NAME)
-	return configFile
-}
+// A ConfigFileProvider is a function returning either:
+//  1. A file name (such as the actual configuration file)
+//  2. A string containing the contents of a configuration file
+// The default configuration file provider gets the name of
+// the actual AisleRiot configuration file in the home directory.
+type ConfigFileProvider func() string
 
 // Loads the configuration from the default input file. This method can
 // be replaced by a mock object in unit tests
-func GetStatisticString(csgs ...ConfigStringGetter) string {
-	var csg func() string
-	if len(csgs) == 0 {
-		csg = DefaultConfigGetter
+func GetStatisticString(provider ...ConfigFileProvider) (string, error) {
+
+	// Get a function that will provide the contents of the config file
+	var configFileProvider func() string
+	if len(provider) == 0 {
+		configFileProvider = func () string {
+			home, _ := os.UserHomeDir()
+			configFile := filepath.Join(home, CONFIG_FILE_NAME)
+			return configFile
+		}		
 	} else {
-		csg = csgs[0]
+		configFileProvider = provider[0]
 	}
-	configFile := csg()
+
+	// Use that function to load the configuration .ini file
+	configFile := configFileProvider()
 	config := ini.Load(configFile)
-	section, _ := config.GetSection("freecell.scm")
-	statisticString, _ := section.GetValue("Statistic")
-	return statisticString
+
+	// Get the freecell section and the statistic string from it
+	var statisticString string
+	section, err := config.GetSection("freecell.scm")
+	if err == nil {
+		statisticString, err = section.GetValue("Statistic")
+	}
+	return statisticString, err
 }
--- data_provider_test.go
+++ data_provider_test.go
@@ -4,15 +4,40 @@ import "testing"
 
 func TestGetStatisticString(t *testing.T) {
 	tests := []struct {
-		name string
-		want string
+		name     string
+		provider func() string
+		want     string
 	}{
-		{"good", "124;153;88;363;"},
+		{"current",
+			func() string {
+				return `[Aisleriot Config]
+ShowToolbar=true
+ShowStatusbar=true
+Sound=false
+Animations=true
+Recent=freecell;klondike;
+Variation=freecell.scm
+ClickToMove=false
+Theme=gnomangelo_bitmap.svgz
+
+[freecell.scm]
+Statistic=4;9;144;208;
+
+[klondike.scm]
+Statistic=0;1;0;0;
+			`
+			},
+			"4;9;144;208;"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GetStatisticString(); got != tt.want {
-				t.Errorf("GetStatisticString() = %v, want %v", got, tt.want)
+			have, err := GetStatisticString(tt.provider)
+			if err != nil {
+				t.Error(err)
+			}
+			want := tt.want
+			if have != want {
+				t.Errorf("%s: have=%q, want=%q", tt.name, have, want)
 			}
 		})
 	}
