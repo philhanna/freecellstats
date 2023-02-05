61762c8c04f399ca0e9cd25014b257d74236540f
Phil Hanna <ph1204@gmail.com>
2023-02-05 14:23:48 -0500

    round percentage in constructor

--- go/statistics.go
+++ go/statistics.go
@@ -1,5 +1,10 @@
 package freecell
 
+import (
+	"fmt"
+	"math"
+)
+
 // Captures the wins, losses, and percentages
 type Statistics struct {
 	wins   int
@@ -19,9 +24,16 @@ func NewStatistics(wins, total, best, worst int) *Statistics {
 	stats.worst = worst
 	stats.losses = total - wins
 	if total != 0 {
-		stats.pct = float64(wins) / float64(total)
+		stats.pct = math.Round(100.0 * float64(wins) / float64(total))
 	}
 	return stats
+}
+
+// Returns a string representation of the structure
+func (stat Statistics) String() string {
+	sb := fmt.Sprintf("wins:%d, total:%d, best:%d, worst:%d, losses:%d, pct:%f.0",
+		stat.wins, stat.total, stat.best, stat.worst, stat.losses, stat.pct)
+	return sb
 }
 
 // Finds the value of the winning percentage
