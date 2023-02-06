1adc42e792c786f2b656a746aa768d4d37a95657
Phil Hanna <ph1204@gmail.com>
2023-02-05 21:02:44 -0500

    Added unit test for wins to next higher

--- statistics_test.go
+++ statistics_test.go
@@ -1,30 +1,13 @@
 package freecell
 
 import (
-	// "fmt"
 	"math"
 	"testing"
 )
 
-func TestPercent(t *testing.T) {
-	type args struct {
-		wins   int
-		losses int
-	}
-	tests := []struct {
-		name string
-		args args
-		want float64
-	}{
-		{"good", args{99, 150}, 40},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Percent(tt.args.wins, tt.args.losses); math.Round(got) != tt.want {
-				t.Errorf("Percent() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+func getTestData() *Statistics {
+	stat := NewStatisticsFromString("99;150;144;208;")
+	return stat
 }
 
 func TestNewStatistics(t *testing.T) {
@@ -58,5 +41,47 @@ func TestNewStatistics(t *testing.T) {
 		if wantString != haveString {
 			t.Errorf("%v: want=%v,have=%v", tt.name, wantString, haveString)
 		}
+	}
+}
+
+func TestPercent(t *testing.T) {
+	type args struct {
+		wins   int
+		losses int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{"good", args{99, 150}, 40},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Percent(tt.args.wins, tt.args.losses); math.Round(got) != tt.want {
+				t.Errorf("Percent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStatistics_WinsToNextHigher(t *testing.T) {
+	tests := []struct {
+		name   string
+		statString string
+		want   int
+	}{
+		{"Normal", "99;150;144;208;", 3},
+		{"No losses", "99;99;144;208;", 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			stat := NewStatisticsFromString(tt.statString)
+			have := stat.WinsToNextHigher()
+			want := tt.want
+			if have != want {
+				t.Errorf("have=%d,want=%d", have, want)
+			}	
+		})
 	}
 }
