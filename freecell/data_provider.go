b14837d93b68b49442230464817be8f1eeafc066
Phil Hanna <ph1204@gmail.com>
2023-02-08 22:01:48 -0500

    Deleted ConfigFileProvider type - not needed

--- freecell/data_provider.go
+++ freecell/data_provider.go
@@ -11,26 +11,33 @@ const (
 	CONFIG_FILE_NAME = ".config/gnome-games/aisleriot"
 )
 
-// A ConfigFileProvider is a function returning either:
-//  1. A file name (such as the actual configuration file)
-//  2. A string containing the contents of a configuration file
-// The default configuration file provider gets the name of
-// the actual AisleRiot configuration file in the home directory.
-type ConfigFileProvider func() string
 
 // Loads the configuration from the default input file. This method can
 // be replaced by a mock object in unit tests
-func GetStatisticString(provider ...ConfigFileProvider) (string, error) {
+//
+// A configuration file provider is a function returning either:
+//  1. A file name (such as the actual configuration file)
+//  2. A string containing the contents of a configuration file
+// The go-ini package will accept either one.
+//
+// The default configuration file provider gets the name of
+// the actual AisleRiot configuration file in the home directory.
+func GetStatisticString(provider ...func() string) (string, error) {
 
 	// Get a function that will provide the contents of the config file
 	var configFileProvider func() string
-	if len(provider) == 0 {
-		configFileProvider = func () string {
+
+	// If no alternate provider was found, use the one relative to
+	// the actual home directory.  Otherwise, use the provider specified
+	// on the command line
+	switch {
+	case len(provider) == 0:
+		configFileProvider = func() string {
 			home, _ := os.UserHomeDir()
 			configFile := filepath.Join(home, CONFIG_FILE_NAME)
 			return configFile
-		}		
-	} else {
+		}
+	default:
 		configFileProvider = provider[0]
 	}
 
